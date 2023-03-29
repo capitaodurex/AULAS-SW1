4dcae457b9e4a650cbc2ea7d0c63bce14c3e7411
Luciano Barros <100379738+capitaodurex@users.noreply.github.com>
2023-03-29 16:26:54 -0300

    Add files via upload

---
+++ atividade-métodoacessor/Produto.cs
@@ -0,0 +1,24 @@
+﻿namespace atividade_produto;
+class Produto
+{
+    private string? Nome{get;set;}
+
+    private double Preco{get;set;}
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome );
+        Console.WriteLine("Preço: " + this.Preco);
+    }
+
+     public void AlteraPreco(double preco){
+        if(preco >= 0){
+            this.Preco = preco;
+        }else{
+            Console.WriteLine("VALOR INVÁLIDO");
+        }
+    }
+
+      public void AlteraNome(string nome){
+       this.Nome = nome; 
+    }
+}
