24b0e6472abef74dfc79471c7f19ea5614771943
Luciano Barros <100379738+capitaodurex@users.noreply.github.com>
2023-03-29 16:58:04 -0300

    Add files via upload

---
+++ atividade-construtor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace atividade_construtor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Produto p1 = new Produto(99);
+        Produto p1 = new Produto("ABC", 10);
+
+        p1.MostraDados();
+    }
+}
